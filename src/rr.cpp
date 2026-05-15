a4d26c639ddea1790f4faf400651781ab85a0a84
Matthew Yang <matthew.yang204@gmail.com>
2026-05-15 12:12:03 -0700

    When RR gets failure, also open log before crashing

--- src/rr.cpp
+++ src/rr.cpp
@@ -3,9 +3,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char* writeToLogFile(const char* expr)
+{
+    char* path = new char[MAX_PATH];
+
+    GetTempPathA(MAX_PATH, path);
+    lstrcatA(path, "wordpad-crash-log.txt");
+
+    FILE* f = fopen(path, "a");
+    if (f)
+    {
+        fprintf(f, expr);
+        fclose(f);
+    }
+
+    return path;
+}
+
 static void RRLogFailure(const char* expr, const char* file, int line)
 {
-    fprintf(stderr, "[RRAssert] Failed: %s (%s:%d)\n", expr, file, line);
+    char* msg = new char[512];
+    _snprintf_s(
+        msg,
+        512,
+        _TRUNCATE,
+        "[RRAssert] Failed: %s (%s:%d)\n",
+        expr,
+        file,
+        line
+    );
+
+    fprintf(stderr, msg);
+    char* path = writeToLogFile(msg);
+
+    ShellExecuteA(
+        NULL,
+        "open",
+        "notepad.exe",
+        path,
+        NULL,
+        SW_SHOW
+    );
 }
 
 void RRAssertFail(const char* expr, const char* file, int line)
