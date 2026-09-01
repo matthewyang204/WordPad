80921adb4955a8fc9428bf46cf6be9c64810ec5b
Matthew Yang <matthew.yang204@gmail.com>
2026-09-01 09:18:06 -0700

    Disable all this useless debugging code

--- src/TaskPane.cpp
+++ src/TaskPane.cpp
@@ -57,29 +57,29 @@ int CTaskPane::OnCreate(LPCREATESTRUCT lpCreateStruct)
 	AddTask (nPage1Gr2, _T("Blank Document"), 1, ID_FILE_NEW);
 
 	AddTask (nPage1Gr2, _T("Blank Web Page"), 2, ID_DUMMY);
-	AddSeparator (nPage1Gr2);
-	AddTask (nPage1Gr2, _T("Blank E-mail message"), 3, ID_DUMMY);
+	// AddSeparator (nPage1Gr2);
+	// AddTask (nPage1Gr2, _T("Blank E-mail message"), 3, ID_DUMMY);
 
-	int nPage1Gr3 = AddGroup (_T("New from existing document"));
+	// int nPage1Gr3 = AddGroup (_T("New from existing document"));
 
-	AddTask (nPage1Gr3, _T("Choose document..."), 4, ID_DUMMY);
+	// AddTask (nPage1Gr3, _T("Choose document..."), 4, ID_DUMMY);
 
-	int nPage1Gr4 = AddGroup (_T("New from template"));
+	// int nPage1Gr4 = AddGroup (_T("New from template"));
 
-	AddTask (nPage1Gr4, _T("General Templates..."), 5, ID_DUMMY);
-	AddTask (nPage1Gr4, _T("Templates on my Web Sites..."), 6, ID_DUMMY);
-	AddTask (nPage1Gr4, _T("Templates on Microsoft.com..."), 7, ID_DUMMY);
+	// AddTask (nPage1Gr4, _T("General Templates..."), 5, ID_DUMMY);
+	// AddTask (nPage1Gr4, _T("Templates on my Web Sites..."), 6, ID_DUMMY);
+	// AddTask (nPage1Gr4, _T("Templates on Microsoft.com..."), 7, ID_DUMMY);
 
-	int nPage1Gr5 = AddGroup (_T(""), TRUE /* Group at bottom */);
+	// int nPage1Gr5 = AddGroup (_T(""), TRUE /* Group at bottom */);
 
-	AddTask (nPage1Gr5, _T("Add Network Place..."), 8, ID_DUMMY);
-	AddTask (nPage1Gr5, _T("Help..."), 9, ID_DUMMY);
-	AddLabel (nPage1Gr5, _T("Label"));
+	// AddTask (nPage1Gr5, _T("Add Network Place..."), 8, ID_DUMMY);
+	// AddTask (nPage1Gr5, _T("Help..."), 9, ID_DUMMY);
+	// AddLabel (nPage1Gr5, _T("Label"));
 
 	//--------
 	// Page 2:
 	//--------
-	int nPage2 = AddPage (_T("Basic Search"));
+	/*int nPage2 = AddPage (_T("Basic Search"));
 
 	int nPage2Gr1 = AddGroup (nPage2, _T("Search for:"));
 
@@ -88,41 +88,41 @@ int CTaskPane::OnCreate(LPCREATESTRUCT lpCreateStruct)
 	int nPage2Gr3 = AddGroup (nPage2, _T("See also:"));
 
 	AddTask (nPage2Gr3, _T("Advanced Search"), 9, ID_DUMMY);
-	AddTask (nPage2Gr3, _T("Find in this document..."), 9, ID_DUMMY);
+	AddTask (nPage2Gr3, _T("Find in this document..."), 9, ID_DUMMY);*/
 
 	//--------
 	// Page 3:
 	//--------
-	int nPage3 = AddPage (_T("Custom page"));
+	//int nPage3 = AddPage (_T("Custom page"));
 
-	int nPage3Gr1 = AddGroup (nPage3, _T("Folders"));
+	//int nPage3Gr1 = AddGroup (nPage3, _T("Folders"));
 
-	if (!CreateTreeWindow())
-	{
-		TRACE0("Failed to create the custom window\n");
-		return -1;      // fail to create
-	}
+	//if (!CreateTreeWindow())
+	//{
+	//	TRACE0("Failed to create the custom window\n");
+	//	return -1;      // fail to create
+	//}
 
-	AddWindow (nPage3Gr1, m_wndTree.GetSafeHwnd (), 65);
-	AddTask (nPage3Gr1, _T("My favorites..."), 0, ID_DUMMY);
+	//AddWindow (nPage3Gr1, m_wndTree.GetSafeHwnd (), 65);
+	//AddTask (nPage3Gr1, _T("My favorites..."), 0, ID_DUMMY);
 
-	int nPage3Gr2 = AddGroup (nPage3, _T("Enter your text"));
+	//int nPage3Gr2 = AddGroup (nPage3, _T("Enter your text"));
 
-	CRect rectDummy;
-	rectDummy.SetRectEmpty ();
-	DWORD dwEditStyle = WS_CHILD | WS_VISIBLE | WS_BORDER | 
-		ES_AUTOHSCROLL | ES_AUTOVSCROLL | ES_MULTILINE | ES_WANTRETURN;
-	m_Font.CreateStockObject (DEFAULT_GUI_FONT);
-	if (!m_wndEdit.Create (dwEditStyle, rectDummy, this, (UINT)-1))
-	{
-		TRACE0("Failed to create the edit window\n");
-		return -1;      // fail to create
-	}
-	m_wndEdit.SetFont (&m_Font);
-	m_wndEdit.SetWindowText(
-		_T("This is the standard multiline Edit Box.\r\nHere you can type your text."));
-	
-	AddWindow (nPage3Gr2, m_wndEdit.GetSafeHwnd (), 65);
+	//CRect rectDummy;
+	//rectDummy.SetRectEmpty ();
+	//DWORD dwEditStyle = WS_CHILD | WS_VISIBLE | WS_BORDER | 
+	//	ES_AUTOHSCROLL | ES_AUTOVSCROLL | ES_MULTILINE | ES_WANTRETURN;
+	//m_Font.CreateStockObject (DEFAULT_GUI_FONT);
+	//if (!m_wndEdit.Create (dwEditStyle, rectDummy, this, (UINT)-1))
+	//{
+	//	TRACE0("Failed to create the edit window\n");
+	//	return -1;      // fail to create
+	//}
+	//m_wndEdit.SetFont (&m_Font);
+	//m_wndEdit.SetWindowText(
+	//	_T("This is the standard multiline Edit Box.\r\nHere you can type your text."));
+	//
+	//AddWindow (nPage3Gr2, m_wndEdit.GetSafeHwnd (), 65);
 	return 0;
 }
 
