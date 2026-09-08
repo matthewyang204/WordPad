6a2d99dfc48a76f4137bfda8e44d22e7d4be9400
Matthew Yang <matthew.yang204@gmail.com>
2026-09-08 09:28:48 -0700

    Create a table.cpp for creating tables

---
+++ src/table.cpp
@@ -0,0 +1,12 @@
+// table.cpp: Table-related functions
+// Contains the creator dialog code
+// (C) 2025-2026 Matthew Yang
+// Licensed under the MIT license
+
+#include "stdafx.h"
+#include "table.h"
+
+class CTableDropDialog : public CMFCToolBarComboBoxButton
+{
+	
+};
